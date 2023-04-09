153470796f12c38090b54b9db66025a8ec39d675
devang-marvania <51920789+devang-marvania@users.noreply.github.com>
2023-04-08 22:22:03 -0400

    Added recursive binary search solution

--- week4_divide_and_conquer/1_binary_search/binary_search.py
+++ week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -1,5 +1,32 @@
-def binary_search(keys, query):
-    # write your code here
+def binary_search(keys, query,low,high):
+    # Binary search function takes sorted array of numbers as input(keys), along with a search item(query) and lower and higher index of keys within which search 
+    #needs to be made. If query matches with any items in keys, then return corrosponding index, otherwise -1
+
+    #Check if query is within the input array by checking at bounds
+    if query<keys[low] or query>keys[high]:
+        return -1
+    
+    #Recursive Binary search implementation starts below
+
+    #In case lower index gets bigger than higher index(could happen by doing changing lower bound to mid+1)
+    if high<low:
+        return low-1
+    
+    #Look at middle of array and depending on query and keys[mid] relation, change the upper and lower bounds
+    mid=int(low+(high-low)/2)
+
+    
+    if query == keys[mid]:
+        return mid
+    
+    elif query<keys[mid]:
+        return binary_search(keys,query,low,mid-1)
+    
+    elif query>keys[mid]:
+        return binary_search(keys,query,mid+1,high)
+
+    else:
+        return -1
 
 
 if __name__ == '__main__':
@@ -11,5 +38,8 @@ if __name__ == '__main__':
     input_queries = list(map(int, input().split()))
     assert len(input_queries) == num_queries
 
+    high_idx=num_keys-1
+    low_idx=0
+
     for q in input_queries:
-        print(binary_search(input_keys, q), end=' ')
+        print(binary_search(input_keys, q,low_idx,high_idx), end=' ')
