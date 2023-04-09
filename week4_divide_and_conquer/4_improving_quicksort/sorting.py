f31cabf2810e578bc2984a6881b1348050ae0eae
devang-marvania <51920789+devang-marvania@users.noreply.github.com>
2023-04-09 16:19:35 -0400

    added improved quick sort algorithm

--- week4_divide_and_conquer/4_improving_quicksort/sorting.py
+++ week4_divide_and_conquer/4_improving_quicksort/sorting.py
@@ -1,9 +1,35 @@
 from random import randint
 
+#Implemented a partition function which returns m1 and m2 indexes. 
+#The region from left to m-1: less than pivot, m1 to m2: equal to pivot, m2+1 to right: greater than pivot
 
 def partition3(array, left, right):
-    # write your code here
 
+    #Initialize variables
+    pivot=array[left]
+    m1=left
+    m2=right
+    i=left
+
+    #Run the loop till all elements are compared by comparing against m2
+    while i<=m2:
+
+        #Adjust to left to m1-1 region
+        if array[i]<pivot:
+            array[i],array[m1]=array[m1],array[i]
+            i=i+1
+            m1=m1+1
+
+        #Adjust the m2+1 to right region    
+        elif array[i]>pivot:
+            array[i],array[m2]=array[m2],array[i]
+            m2=m2-1
+
+        #Adjust the m1 to m2 region    
+        else:
+            i=i+1
+
+    return m1,m2
 
 def randomized_quick_sort(array, left, right):
     if left >= right:
