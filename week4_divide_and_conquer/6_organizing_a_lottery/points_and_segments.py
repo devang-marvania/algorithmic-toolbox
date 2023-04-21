c2d576f1638614458a03210aa5a40b8899e0c64d
devang-marvania <51920789+devang-marvania@users.noreply.github.com>
2023-04-20 23:50:59 -0400

    Optimized the algorithm for points and cover

--- week4_divide_and_conquer/6_organizing_a_lottery/points_and_segments.py
+++ week4_divide_and_conquer/6_organizing_a_lottery/points_and_segments.py
@@ -1,6 +1,38 @@
 from sys import stdin
 from random import randint
-import bisect
+
+
+
+
+def points_cover_optimized(starts,ends,points):
+
+    point_list=[]
+
+    #Create a common list of all numbers: start points, end points and points. Attach the label "s" or "e" or "p" to denote whether point is start, end or point.
+    #These labels will be used later to run a counter. For the point, save point_idx as well to appropriately maintain score
+    point_list =[(start_point,"s") for start_point in starts]+[(end_point,"e") for end_point in ends]+[(point,"p",point_idx) for point_idx,point in enumerate(points)]
+
+    #Sort the combined point list in ascending order. If two points are same then put the start first, then point and then end point.
+    point_list_sorted=sorted(point_list,key=lambda x:(x[0],-ord(x[1])))
+
+    #A running score variable
+    segment_score=0
+
+    #Zero array to keep score of each point
+    counter_array=[0]*len(points)
+
+    for i,tup in enumerate(point_list_sorted):
+        if tup[1]=="s":
+            #Increase score when segment starts
+            segment_score=segment_score+1
+        if tup[1]=="e":
+            #Decrease score when segment ends
+            segment_score=segment_score-1
+        if tup[1]=="p":
+            #When point is encountered, save the score in counter array
+            counter_array[tup[2]]=segment_score
+
+    return counter_array                
 
 
 
@@ -11,63 +43,6 @@ import bisect
 
 
 
-
-def points_cover(starts,ends,points):
-    #assert len(starts)==len(ends)
-
-    #segment_id=[i for i in range(len(starts))]
-    segment_list=[(starts[i],ends[i]) for i in range(len(starts))]
-    segment_list_sorted=sorted(segment_list,key=lambda x:x[0])
-
-    result=[]
-
-    for point in points:
-        score=search_sorted_segment_list(segment_list_sorted,point)
-        result.append(score)
-
-     
-
-    return result
-
-def search_sorted_segment_list(segment_list_sorted,point):
-
-    segment_start_sorted=[segment[0] for segment in segment_list_sorted]
-    right_segment_idx=bisect.bisect(segment_start_sorted,point)
-
-  
-
-    segment_shortlist_by_start=segment_list_sorted[:right_segment_idx]
-    segment_shortlist_by_end=[segment for segment in segment_shortlist_by_start if point<=segment[1]]
-
-    return len(segment_shortlist_by_end)
-
-
-#input_starts=[0,-3,7]
-#input_ends=[5,2,10]
-#input_points=[1,6]
-
-#print(points_cover(starts,ends,points))
-
-
-
-
-
-'''
-if __name__ == '__main__':
-    #data = list(map(int, stdin.read().split()))
-    #n, m = data[0], data[1]
-    #input_starts, input_ends = data[2:2 * n + 2:2], data[3:2 * n + 2:2]
-    #input_points = data[2 * n + 2:]
-
-    #output_count = points_cover_naive(input_starts, input_ends, input_points)
-    #print(*output_count)
-
-    #points_cover_naive(input_starts, input_ends, input_points)
-    output_count=points_cover(input_starts, input_ends, input_points)
-    print(*output_count)
-
-    #print()
-'''
 
 
 if __name__ == '__main__':
@@ -76,11 +51,7 @@ if __name__ == '__main__':
     input_starts, input_ends = data[2:2 * n + 2:2], data[3:2 * n + 2:2]
     input_points = data[2 * n + 2:]
 
-    output_count = points_cover(input_starts, input_ends, input_points)
+    output_count = points_cover_optimized(input_starts, input_ends, input_points)
     
 
-    points_cover(*output_count)
-
-
-
-
+    print(*output_count)
