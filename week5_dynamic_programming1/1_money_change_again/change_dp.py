ce101d4876dc9ce5ab915e8f15bb857ed4e63097
devang-marvania <51920789+devang-marvania@users.noreply.github.com>
2023-05-06 11:56:45 -0400

    added solution for money change

--- week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -1,7 +1,24 @@
 def change(money):
-    # write your code here
+    #Create a list with some safe values(infinity here) as default. We will replace infinity with actual change value eventually. Here MinNumCoins[idx]=value, 
+    # idx represents the money to be changed, value represents the min number of coins needed to replace original money
+    MinNumCoins=[float('inf')]* (money+1)
+    
+    #list of available denominations
+    coins=[1,3,4]
 
-    return money
+    #Min number of coins to change 0 dollar is 0, reflecting same here
+    MinNumCoins[0]=0
+
+    #Loop from 1 to money and whenever change amount is more than coin value, find out the min number of coins using previously stored MinNumCoins value
+    for m in range(1,money+1):
+        for coin in coins:
+            if m>=coin:
+                NumCoins=MinNumCoins[m-coin]+1
+                if NumCoins<MinNumCoins[m]:
+                    MinNumCoins[m]=NumCoins
+
+
+    return MinNumCoins[money]
 
 
 if __name__ == '__main__':
